751a0e77ef6b68b7c28e7eb009ba81f633ad90a6
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-23 15:55:16 +0600

    finished practice problem 2

--- practice.js
+++ practice.js
@@ -21,4 +21,38 @@ let data = {
     }
 }
 
-console.log(data.Sophia.study[1].secondary[1].location);
+// console.log(data.Sophia.study[1].secondary[1].location);
+
+/* ---------------------------------------------------------------- */
+
+
+// task-2: console .log  output: Petersburg, Herry
+
+let students = {
+    2222: {
+        name: "Jack",
+        section: "C",
+        class: "IX",
+        address: {
+            "building no": 12,
+            "street": "St. Jonson",
+            "city": "Petersburg",
+            "country": "UK"
+        }
+    },
+    3333: {
+        name: "Herry",
+        section: "D",
+        class: "X",
+        address: {
+            "building no": 85,
+            "street": "DC road",
+            "city": "Kachukhet",
+            "country": "Bangladesh"
+        }
+    }
+}
+
+
+// console.log(students['2222']['address'].city, students['3333'].name);
+console.log(`${students['2222']['address'].city}, ${students['3333'].name}`);
